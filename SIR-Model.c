a1910ae9fcd057877e0fa2109d1aa18ae5dfdaf0
william <william.otto.hjorth@gmail.com>
2025-11-03 08:57:18 +0100

    printer tal ud der ændrer sig hver dag
    første udkast

--- SIR-Model.c
+++ SIR-Model.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
 #include <math.h>
 
-    int time = 0;
+float Differentiationer(float S, float I, float R);
+
 float S0 = 500;
- 
-int S;
+
+float S;
 float I0 = 2;
-float Is;
-int R0=0;
+float I;
+float R0 = 0;
 float R;
 float betaYAY = 1.4247;
 float gammaYAY = 0.143;
 float days = 1.0;
-int main (void)
+float N = 502;
+int main(void)
 
 {
 
-float function(float x);
+    Differentiationer(S0, I0, R0);
+    // printf("%d",S0);
 
- 
- function(S0);
- // printf("%d",S0);
-
-    
- // printf("skriv parameter for beta og gamma");
-return 0;
+    // printf("skriv parameter for beta og gamma");
+    return 0;
 }
 
- float function(float x){
+float Differentiationer(float S, float I, float R)
+{
 
-    for (int i = 0; i <100; i++)
+    for (int n = 0; n < 100; n++)
     {
-    printf("dag : %d var der : %.2f suceetible\n", i, x);
+        float dt = 1.0f;
+        float dSdT = -betaYAY * S * I / N;
+        float dIdT = betaYAY * S / N - gammaYAY * I;
+        float dRdt = gammaYAY * I;
 
-    float dsdt = -betaYAY*I0*x;
-       x = x+ dsdt*days;
-        
+        S += dSdT * dt;
+        I += dIdT * dt;
+        R += dRdt * dt;
+
+        printf(" S = %.2f\n I = %.2f\n R = %.2f\n", S, I, R);
     }
- 
 }
