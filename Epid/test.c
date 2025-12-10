00ac46eec91607050ea82eabd5ea54418c0babee
Andreas Hertel Bidstrup <andreas.h.bidstrup@gmail.com>
2025-12-10 22:57:37 +0100

    Add files via upload

---
+++ Epid/test.c
@@ -0,0 +1,17 @@
+#include "epid.h"
+
+
+
+int main () {
+  SEIHRS_model tekstfil[2];
+  SEIHRS_model tekstfil_orig[2];
+
+  //Ararnge
+  
+
+  //Act
+  simulerEpidemi (.....);
+   
+  //Assert
+  
+}
